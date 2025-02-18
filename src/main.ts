59ac4fd9ecb8491edda667a752a74bc55b06c22f
mayonezny <borischubuhchiev@mail.ru>
2025-02-18 15:12:47 +0300

    пофиксили лок на размер файла

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import multipart, { fastifyMultipart } from '@fastify/multipart';
-
+// eslint-disable-next-line no-magic-numbers
+const maxSize = 3 * 1024 * 1024;
 const PORT = 80;
 async function bootstrap() {
   // eslint-disable-next-line max-len
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { bodyParser: false });
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ bodyLimit: maxSize }), { bodyParser: false });
   await app.register(fastifyMultipart, {
-    attachFieldsToBody: true, // Включает поддержку multipart
+    attachFieldsToBody: true,
+    // Включает поддержку multipart
   });
 
   await app.listen(process.env.PORT ?? PORT);
